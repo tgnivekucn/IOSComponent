19c7af31f2443b7187a5949b09ac0de2a1235cd3
Kuang-Yu Nien <tgnivekucn@gmail.com>
2019-12-06 15:56:08 +0800

    simple tableview

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1")
+        cell?.textLabel?.text = "test"
+        return cell!
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("select row: \(indexPath.row)")
     }
 
-
 }
+
+
 
