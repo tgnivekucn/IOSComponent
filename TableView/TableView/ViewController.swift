73d68be53c5e69f619ce1174f18db641a9420938
Kuang-Yu Nien <tgnivekucn@gmail.com>
2019-12-06 09:49:59 +0800

    init tableview project

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Kuangyu Nien on 2019/12/6.
+//  Copyright © 2019 Kuangyu Nien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
