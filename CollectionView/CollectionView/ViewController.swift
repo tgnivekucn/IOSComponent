89dda10f1f0554964b2b5af4f5f38e031ca426d4
Kuang-Yu <tgnivekucn@gmail.com>
2019-12-09 00:36:44 +0800

    show CollectionView with basic text cell

--- CollectionView/CollectionView/ViewController.swift
+++ CollectionView/CollectionView/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mCollectionView.dataSource = self
+        mCollectionView.delegate = self
+        mCollectionView.dragInteractionEnabled = true
+        mCollectionView.dragDelegate = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("didSelectItemAt row: \(indexPath.row)")
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        print("didDeselectItemAt row: \(indexPath.row)")
+    }
+}
+
+extension ViewController: UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        
+        return [] //note: must return non-empty array for starting drag action
+    }
+}
