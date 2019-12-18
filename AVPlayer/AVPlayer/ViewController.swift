ee2873192fed9b06d71726e07b54189999ae4588
Kuang-Yu Nien <tgnivekucn@gmail.com>
2019-12-18 11:49:21 +0800

    add default player by AVPlayerViewController

--- AVPlayer/AVPlayer/ViewController.swift
+++ AVPlayer/AVPlayer/ViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import AVFoundation
+import AVKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,6 +16,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pressBtnAction(_ sender: UIButton) {
+        playVideo()
+    }
+    
+    
+    private func playVideo() {
+        guard let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else {
+            return
+        }
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
 
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        
+        let controller = AVPlayerViewController()
+        controller.player = player
+
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
 }
 
